c52c6070897e5a501e5eadc51ee03331bcf5eb52
wanni0928 <chlgudwnd123@gmail.com>
2021-01-04 21:20:57 +0900

    AuthenticationContext & ThreadLocal

--- src/main/java/com/wannistudio/security/form/SampleController.java
+++ src/main/java/com/wannistudio/security/form/SampleController.java
@@ -1,5 +1,8 @@
 package com.wannistudio.security.form;
 
+import com.wannistudio.security.account.AccountContext;
+import com.wannistudio.security.account.AccountRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,7 +10,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.security.Principal;
 
 @Controller
+@RequiredArgsConstructor
 public class SampleController {
+
+    private final SampleService sampleService;
+    private final AccountRepository accountRepository;
 
     @GetMapping("/")
     public String index(Model model, Principal principal) {
@@ -29,6 +36,8 @@ public class SampleController {
     @GetMapping("/dashboard")
     public String dashboard(Model model, Principal principal) {
         model.addAttribute("message", "hello " + principal.getName());
+        AccountContext.setAccount(accountRepository.findByUsername(principal.getName()));
+        sampleService.dashboard();
         return "dashboard";
     }
 
